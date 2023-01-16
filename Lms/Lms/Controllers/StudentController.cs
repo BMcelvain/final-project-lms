9b5a148a12c10bc8e5b1d88ef3718dfd40d45de4
Brett McElvain <Brett.McElvain@vu.com>
2023-01-15 22:20:06 -0600

    Initial Setup of LMS

---
+++ Lms/Lms/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lms.Controllers
+{
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+    }
+}
