9eec90771bdecf3ee8cdeb3da013381bdf80c678
Kristina Palmer <thekristinadawn@gmail.com>
2023-03-03 16:00:39 -0600

    Development

--- Lms/Lms/Startup.cs
+++ Lms/Lms/Startup.cs
@@ -33,7 +33,6 @@ namespace Lms
             services.AddScoped<ITeacherDao, TeacherDao>();
             services.AddScoped<IStudentDao, StudentDao>();
             services.AddScoped<IStudentEnrollmentDao, StudentEnrollmentDao>();
-            services.AddScoped<IStudentActiveEnrollmentDao, StudentActiveEnrollmentDao>();
             services.AddScoped<IAddStudentToCourseDao, AddStudentToCourseDao>();
 
 
